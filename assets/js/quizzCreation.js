97a880519798d85b21074a36555e6aba4db8b7df
Alex Souza <spfc.alex4@gmail.com>
2022-04-22 00:58:22 -0300

    Implementada lógica responsável por pegar dados de perguntas dos quizzes, e montar o layout.

--- assets/js/quizzCreation.js
+++ assets/js/quizzCreation.js
@@ -2,9 +2,13 @@
 
 let creationActualStep = 0;
 let creationStepsElements = [];
+const formCreationData = {};
 const forwardBtn = document.querySelector('.forward-btn');
 const quizzCreationTitle = quizzCreationScreen.querySelector('.form-title h3');
 const quizzCreationForm = quizzCreationScreen.querySelector('form');
+const quizzCreationStep1 = quizzCreationScreen.querySelector('.step1');
+const quizzCreationStep2 = quizzCreationScreen.querySelector('.step2');
+const quizzCreationStep3 = quizzCreationScreen.querySelector('.step3');
 
 // Função responsável por carregar em um array todos os elementos do HTML que compõem o formulário de criação de um quiz.
 function loadSteps(){
@@ -23,12 +27,12 @@ function validateQuizzTitle(title){
     return (title.length >= 20 && title.length <= 65);
 }
 
-function validateQuizzQuestions(questionsQty){
-    return (parseInt(questionsQty) >= 3);
+function validateQuizzQuestionsQty(questionsQty){
+    return (questionsQty >= 3);
 }
 
 function validateQuizzLevels(levelsQty){
-    return (parseInt(levelsQty) >= 2);
+    return (levelsQty >= 2);
 }
 
 function validateURL(url){
@@ -40,28 +44,70 @@ function validateURL(url){
 
 }
 
+function getIncorrectAnswers(wrapper){
+    
+    let incorrectAnswers = {
+        texts: [...wrapper.querySelectorAll('.incorrect-answer-text')].map(input => input.value),
+        urls: [...wrapper.querySelectorAll('.incorrect-answer-url')].map(input => input.value)
+    };
+    
+    return incorrectAnswers;
+
+}
+
+function getQuestionsInputs(){
+
+    let questions = [];
+    
+    [...quizzCreationStep2.querySelectorAll('.question-wrapper')].forEach((wrapper, index)=>{
+        
+        questions.push({
+            questionText: wrapper.querySelector('.question-text-input').value,
+            questionColor: wrapper.querySelector('.question-color-input').value,
+            correctAnswer: {
+                text: wrapper.querySelector('.answer-input').value,
+                imgURL: wrapper.querySelector('.answer-url-input').value
+            },
+            incorrectAnswers: getIncorrectAnswers(wrapper)
+        });
+
+    });
+
+    return questions;
+
+}
+
+function validateQuizzQuestionsInputs(){
+
+    const questions = getQuestionsInputs();
+    console.log('questions', questions);
+
+}
+
 function validateStep(){
 
     const formData = getFormData();
 
-    switch (creationActualStep) {
+    if(creationActualStep === 0){
 
-        case 0:
-            
-            const title = formData.get('title');
-            const imgURL = formData.get('imgURL');
-            const questionsQty = formData.get('questionsQty');
-            const levelsQty = formData.get('levelsQty');
-            
-            return (validateQuizzTitle(title) && validateQuizzQuestions(questionsQty) && validateQuizzLevels(levelsQty) && validateURL(imgURL));
+        formCreationData.title = formData.get('title');
+        formCreationData.imgURL = formData.get('imgURL');
+        formCreationData.questionsQty = parseInt(formData.get('questionsQty'));
+        formCreationData.levelsQty = parseInt(formData.get('levelsQty'));
+        
+        if(validateQuizzTitle(formCreationData.title) && validateQuizzQuestionsQty(formCreationData.questionsQty) && validateQuizzLevels(formCreationData.levelsQty) && validateURL(formCreationData.imgURL)){
+            return true;
+        } else {
+            return false;
+        }
 
-        case 1:
-            
-            break;
+    } else if(creationActualStep === 1){
 
-        case 2:
-            
-            break;
+        return validateQuizzQuestionsInputs();
+
+    } else if(creationActualStep === 2){
+
+
 
     }
 
@@ -69,7 +115,6 @@ function validateStep(){
 
 function nextStep(){
 
-    console.log('validou ', validateStep());
     if(validateStep() === true){
 
         hideAllSteps();
@@ -84,6 +129,7 @@ function nextStep(){
                 case 1:
                     quizzCreationTitle.innerText = 'Crie suas perguntas';
                     forwardBtn.innerText = 'Prosseguir para criar níveis';
+                    appendQuestionsToForm();
                     break;
 
                 case 2:
@@ -112,10 +158,87 @@ function displayActualScreen(){
 function controlButtons(){
 
     forwardBtn.addEventListener('click', () => nextStep());
-    
-
     const backHomeBtn = document.querySelector('.back-home-btn');
     backHomeBtn.addEventListener('click', () => changeScreen('lists'));
+
+}
+
+function appendQuestionsToForm(){
+
+    quizzCreationStep1.classList.add('hidden');
+    quizzCreationStep2.classList.remove('hidden');
+
+    for(let i = 0; i < formCreationData.questionsQty; i++){
+
+        quizzCreationStep2.innerHTML += `
+            <div class="form-group collapsed">
+
+                <div class="form-group-header d-flex">
+                    <h5>Pergunta ${i + 1}</h5>
+                    <ion-icon name="create-outline"></ion-icon>
+                </div>
+
+                <div class="question-wrapper">
+                
+                    <div class="input-group form-section">
+                        <label for="">Pergunta ${i + 1}</label>
+                        <input type="text" placeholder="Texto da pergunta" class="question-text-input">
+                        <input type="text" placeholder="Cor de fundo da pergunta" class="question-color-input">
+                    </div>
+
+                    <div class="input-group form-section">
+                        <label for="">Resposta correta</label>
+                        <input type="text" placeholder="Resposta correta" class="answer-input">
+                        <input type="url" placeholder="URL da imagem" class="answer-url-input">
+                    </div>
+
+                    <div class="input-group form-section">
+                        <label for="">Respostas incorretas</label>
+                        <input type="text" placeholder="Resposta incorreta 1" class="incorrect-answer-text">
+                        <input type="url" placeholder="URL da imagem 1" class="incorrect-answer-url">
+                    </div>
+
+                    <div class="input-group form-section">
+                        <input type="text" placeholder="Resposta incorreta 2" class="incorrect-answer-text">
+                        <input type="url" placeholder="URL da imagem 2" class="incorrect-answer-url">
+                    </div>
+
+                    <div class="input-group form-section">
+                        <input type="text" placeholder="Resposta incorreta 3" class="incorrect-answer-text">
+                        <input type="url" placeholder="URL da imagem 3" class="incorrect-answer-url">
+                    </div>
+
+                </div>
+
+            </div>
+        `;
+
+    }
+
+    controlClickOnQuestions();
+    quizzCreationStep2.querySelector('.form-group:first-child').click();
+
+}
+
+function controlClickOnQuestions(){
+
+    [...quizzCreationStep2.querySelectorAll('.form-group .form-group-header')].forEach(element=>{
+
+        element.addEventListener('click', ()=>{
+            
+            if(element.parentNode.classList.contains('collapsed')){
+
+                element.parentNode.classList.remove('collapsed');
+                element.parentNode.classList.add('expanded');
+
+            } else {
+                element.parentNode.classList.add('collapsed');
+                element.parentNode.classList.remove('expanded');
+            }
+
+        });
+
+    });
 
 }
 
