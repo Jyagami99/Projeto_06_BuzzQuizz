8bd17f97909f55d07d950323132f3c0feb45fd0f
Joao Marcelo <jm.castro@usp.br>
2022-04-23 23:20:48 -0300

    revertendo alterações na branch master

--- assets/js/quizzCreation.js
+++ assets/js/quizzCreation.js
@@ -4,11 +4,13 @@ let creationActualStep = 0;
 let creationStepsElements = [];
 const formCreationData = {};
 const forwardBtn = document.querySelector('.forward-btn');
+const backHomeBtn = document.querySelector('.back-home-btn');
 const quizzCreationTitle = quizzCreationScreen.querySelector('.form-title h3');
 const quizzCreationForm = quizzCreationScreen.querySelector('form');
 const quizzCreationStep1 = quizzCreationScreen.querySelector('.step1');
 const quizzCreationStep2 = quizzCreationScreen.querySelector('.step2');
 const quizzCreationStep3 = quizzCreationScreen.querySelector('.step3');
+const finalScreen = quizzCreationScreen.querySelector('.final-screen');
 
 // Função responsável por carregar em um array todos os elementos do HTML que compõem o formulário de criação de um quiz.
 function loadSteps(){
@@ -31,26 +33,35 @@ function validateQuizzQuestionsQty(questionsQty){
     return (questionsQty >= 3);
 }
 
-function validateQuizzLevels(levelsQty){
+function validateQuizzLevelsQty(levelsQty){
     return (levelsQty >= 2);
 }
 
 function validateURL(url){
 
     url = url.trim();
-    let expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
-    let regex = new RegExp(expression);
+    const expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+    const regex = new RegExp(expression);
     return regex.test(url);
 
 }
 
 function getIncorrectAnswers(wrapper){
+
+    const textsArray = [...wrapper.querySelectorAll('.incorrect-answer-text')].map(input => input.value);
+    const urlsArray = [...wrapper.querySelectorAll('.incorrect-answer-url')].map(input => input.value);
     
-    let incorrectAnswers = {
-        texts: [...wrapper.querySelectorAll('.incorrect-answer-text')].map(input => input.value),
-        urls: [...wrapper.querySelectorAll('.incorrect-answer-url')].map(input => input.value)
-    };
+    let incorrectAnswers = [];
     
+    for(let i = 0; i < textsArray.length; i++){
+
+        incorrectAnswers.push({
+            text: textsArray[i],
+            image: urlsArray[i]
+        });
+
+    }
+
     return incorrectAnswers;
 
 }
@@ -66,7 +77,7 @@ function getQuestionsInputs(){
             questionColor: wrapper.querySelector('.question-color-input').value,
             correctAnswer: {
                 text: wrapper.querySelector('.answer-input').value,
-                imgURL: wrapper.querySelector('.answer-url-input').value
+                image: wrapper.querySelector('.answer-url-input').value
             },
             incorrectAnswers: getIncorrectAnswers(wrapper)
         });
@@ -77,10 +88,140 @@ function getQuestionsInputs(){
 
 }
 
+function validateHexColor(hexColor){
+
+    if(hexColor.length !== 7) {
+        return false;
+    }
+
+    hexColor = hexColor.trim();
+    const expression = /^#(?:[0-9a-fA-F]{3,4}){1,2}$/;
+    const regex = new RegExp(expression);
+    return regex.test(hexColor);
+
+}
+
+function validateIncorrectAnswers(incorrectAnswers){
+
+    const filledAnswers = incorrectAnswers.filter(answer=>{
+
+        if(answer.text !== '' && validateURL(answer.image)) {
+            return true;
+        }
+        return false;
+
+    });
+    return filledAnswers;
+
+}
+
 function validateQuizzQuestionsInputs(){
 
     const questions = getQuestionsInputs();
-    console.log('questions', questions);
+    let isValid = true;
+    formCreationData.questions = questions;
+
+    for(let i = 0; i < questions.length; i++){
+
+        const questionText = questions[i].questionText;
+        const questionColor = questions[i].questionColor;
+        const questionCorrectAnswer = questions[i].correctAnswer;
+        const questionIncorrectAnswers = questions[i].incorrectAnswers;
+
+        if(questionText.length < 20) {
+            isValid = false;
+            break;
+        }
+
+        if(validateHexColor(questionColor) === false){
+            isValid = false;
+            break;
+        }
+
+        if(questionCorrectAnswer.text === '' || validateURL(questionCorrectAnswer.image) === false){
+            isValid = false;
+            break;
+        }
+
+        const questionFilledAnswers = validateIncorrectAnswers(questionIncorrectAnswers);
+
+        if(questionFilledAnswers.length === 0){
+            isValid = false;
+            break;
+        }
+
+        formCreationData.questions[i].incorrectAnswers = questionFilledAnswers;
+
+    }
+
+    return isValid;
+
+}
+
+function getLevelsInputs(){
+
+    let levels = [];
+    
+    [...quizzCreationStep3.querySelectorAll('.level-wrapper')].forEach((wrapper, index)=>{
+        
+        levels.push({
+            title: wrapper.querySelector('.level-title').value,
+            minValue: parseInt(wrapper.querySelector('.level-percentage').value),
+            image: wrapper.querySelector('.level-url').value,
+            text: wrapper.querySelector('.level-description').value
+        });
+
+    });
+
+    return levels;
+
+}
+
+function validateQuizzLevelsInputs(){
+    
+    const levels = getLevelsInputs();
+    let isValid = true;
+    let has0PercentageField = false;
+    formCreationData.levels = levels;
+
+    for(let i = 0; i < levels.length; i++){
+
+        const title = levels[i].title;
+        const minValue = levels[i].minValue;
+        const image = levels[i].image;
+        const text = levels[i].text;
+
+        if(title.length < 10) {
+            isValid = false;
+            break;
+        }
+
+        if(minValue < 0 || minValue > 100){
+            isValid = false;
+            break;
+        }
+
+        if(minValue === 0){
+            has0PercentageField = true;
+        }
+
+        if(validateURL(image) === false){
+            isValid = false;
+            break;
+        }
+
+        if(text.length < 30){
+            isValid = false;
+            break;
+        }
+
+    }
+
+    if(has0PercentageField === false) {
+        isValid = false;
+    }
+
+    return isValid;
 
 }
 
@@ -95,7 +236,7 @@ function validateStep(){
         formCreationData.questionsQty = parseInt(formData.get('questionsQty'));
         formCreationData.levelsQty = parseInt(formData.get('levelsQty'));
         
-        if(validateQuizzTitle(formCreationData.title) && validateQuizzQuestionsQty(formCreationData.questionsQty) && validateQuizzLevels(formCreationData.levelsQty) && validateURL(formCreationData.imgURL)){
+        if(validateQuizzTitle(formCreationData.title) && validateQuizzQuestionsQty(formCreationData.questionsQty) && validateQuizzLevelsQty(formCreationData.levelsQty) && validateURL(formCreationData.imgURL)){
             return true;
         } else {
             return false;
@@ -107,7 +248,12 @@ function validateStep(){
 
     } else if(creationActualStep === 2){
 
+        return validateQuizzLevelsInputs();
 
+    } else if(creationActualStep === 3){
+
+        // aqui devemos carregar a página do quiz
+        // necessário ter uma função para exibir um quiz
 
     }
 
@@ -135,6 +281,7 @@ function nextStep(){
                 case 2:
                     quizzCreationTitle.innerText = 'Agora, decida os níveis';
                     forwardBtn.innerText = 'Finalizar Quizz';
+                    appendLevelsToForm();
                     break;
 
             }
@@ -158,8 +305,8 @@ function displayActualScreen(){
 function controlButtons(){
 
     forwardBtn.addEventListener('click', () => nextStep());
-    const backHomeBtn = document.querySelector('.back-home-btn');
     backHomeBtn.addEventListener('click', () => changeScreen('lists'));
+    // ao voltar para a home, devemos recarregar a lista de quizzes, então devemos ter uma função para isso também
 
 }
 
@@ -215,14 +362,49 @@ function appendQuestionsToForm(){
 
     }
 
-    controlClickOnQuestions();
+    controlClickOnFormGroups(quizzCreationStep2);
     quizzCreationStep2.querySelector('.form-group:first-child').click();
 
 }
 
-function controlClickOnQuestions(){
+function appendLevelsToForm(){
 
-    [...quizzCreationStep2.querySelectorAll('.form-group .form-group-header')].forEach(element=>{
+    quizzCreationStep2.classList.add('hidden');
+    quizzCreationStep3.classList.remove('hidden');
+
+    for(let i = 0; i < formCreationData.levelsQty; i++){
+
+        quizzCreationStep3.innerHTML += `
+            <div class="form-group expanded">
+
+                <div class="form-group-header d-flex">
+                    <h5>Nível ${i + 1}</h5>
+                    <ion-icon name="create-outline"></ion-icon>
+                </div>
+
+                <div class="level-wrapper">
+                    <div class="input-group">
+                        <label for="">Nível ${i + 1}</label>
+                        <input type="text" placeholder="Título do nível" class="level-title">
+                        <input type="number" placeholder="% de acerto mínima" class="level-percentage">
+                        <input type="url" placeholder="URL da imagem do nível" class="level-url">
+                        <input type="text" placeholder="Descrição do nível" class="level-description">
+                    </div>
+                </div>
+
+            </div>
+        `;
+
+    }
+
+    controlClickOnFormGroups(quizzCreationStep3);
+    quizzCreationStep3.querySelector('.form-group:first-child').click();
+
+}
+
+function controlClickOnFormGroups(stepWrapper){
+
+    [...stepWrapper.querySelectorAll('.form-group .form-group-header')].forEach(element=>{
 
         element.addEventListener('click', ()=>{
             
@@ -242,11 +424,74 @@ function controlClickOnQuestions(){
 
 }
 
+function parseQuestion(question){
+
+    const questionAnswers = question.incorrectAnswers.map(incorrectAnswer=>{
+        incorrectAnswer.isCorrectAnswer = false;
+        return incorrectAnswer;
+    });
+    question.correctAnswer.isCorrectAnswer = true;
+    questionAnswers.push(question.correctAnswer);
+
+    const obj = {
+        title: question.questionText,
+        color: question.questionColor,
+        answers: questionAnswers
+    };
+
+    return obj;
+
+}
+
+function parseQuizzData(){
+
+    const obj = {
+        title: formCreationData.title,
+        image: formCreationData.imgURL,
+        questions: formCreationData.questions.map(question => parseQuestion(question)),
+        levels: formCreationData.levels
+    };
+
+    return obj;
+
+}
+
 function finishQuizzCreation(){
 
-    console.log('finalizando criação do quizz...');
-    quizzCreationTitle.innerText = 'Seu quizz está pronto!';
-    forwardBtn.innerText = 'Acessar Quizz';
+    const quizzData = parseQuizzData();
+
+    axios.post('https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes', quizzData).then(response=>{
+
+        finalScreen.classList.remove('hidden');
+        const quizzImgEl = finalScreen.querySelector('img');
+        const quizzTitleEl = finalScreen.querySelector('.quizz-gradient h3');
+
+        quizzImgEl.src = quizzData.image;
+        quizzTitleEl.innerText = quizzData.title;
+        quizzCreationTitle.innerText = 'Seu quizz está pronto!';
+        forwardBtn.innerText = 'Acessar Quizz';
+        backHomeBtn.classList.remove('hidden');
+        saveUserQuizz(response.data);
+
+    }).catch(err=>{
+        console.log('err', err, err.response);
+        alert('Ocorreu um erro desconhecido ao salvar o quizz. Tente novamente.');
+    });
+
+}
+
+function saveUserQuizz(quizzData){
+
+    let userQuizzes = localStorage.getItem('quizzes');
+    
+    if(userQuizzes !== null){
+        userQuizzes = JSON.parse(userQuizzes);
+    } else {
+        userQuizzes = [];
+    }
+
+    userQuizzes.push(quizzData);
+    localStorage.setItem('quizzes', JSON.stringify(userQuizzes));
 
 }
 
